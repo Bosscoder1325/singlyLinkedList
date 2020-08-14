3e2e2c3908d7bec4e782943382beff7afa3f1c73
skitwebdev <webdevelopmentskit@gmail.com>
2020-08-14 14:59:26 +0530

    added reverse linked list function

--- src/com/bosscoder/Main.java
+++ src/com/bosscoder/Main.java
@@ -2,203 +2,222 @@ package com.bosscoder;
 
 import java.util.Scanner;
 
-public class Main {
-
+public class Main{
     public static void main(String[] args) {
-	// write your code here
-        Scanner sc = new Scanner(System.in);
         LinkedList list = new LinkedList();
+        Scanner sc = new Scanner(System.in);
         while (true){
-            System.out.println("1. Add item to the list at start");
-            System.out.println("2. Add item to the list at last");
-            System.out.println("3. Add item to the list at position");
-            System.out.println("4. Delete first item from the list");
-            System.out.println("5. Delete last item from the list");
-            System.out.println("6. Delete last item from the list with given position");
-            System.out.println("7. View list");
-            System.out.println("8. Size of the list");
-            System.out.println("9. Exit");
-            System.out.println("\nEnter your choice : ");
-            int choice = sc.nextInt();
-            int pos,val;
-            switch (choice){
-                case 1:
+            int ch;
+            System.out.println("1. insert element at first");
+            System.out.println("2. insert element at last");
+            System.out.println("3. get size of list");
+            System.out.println("4. view list");
+            System.out.println("5. Exit");
+            System.out.println("6. insert at position");
+            System.out.println("7. delete at first");
+            System.out.println("8. delete at last");
+            System.out.println("9. delete at position");
+            System.out.println("10. reverse list");
+            System.out.println("enter your choice");
+            ch = sc.nextInt();
+            int val,pos;
+            switch (ch){
+                case 1 :
                     System.out.println("enter value");
                     val = sc.nextInt();
-                    list.insertAtFirst(val);
+                    list.insertAtStart(val);
                     break;
                 case 2:
                     System.out.println("enter value");
                     val = sc.nextInt();
-                    list.insertAtLast(val);
+                    list.insertAtEnd(val);
                     break;
-                case 3:
+                case 3: list.getListSize();break;
+                case 4 : list.view();break;
+                case 5 : System.exit(0);break;
+                case 6:
                     System.out.println("enter position");
                     pos = sc.nextInt();
                     System.out.println("enter value");
                     val = sc.nextInt();
                     list.insertAtPosition(val,pos);
                     break;
-                case 4:
-                    list.deleteFirst();
-                    break;
-                case 5:
-                    list.deleteLast();
-                    break;
-                case 6:
-                    System.out.println("enter position");
-                    pos = sc.nextInt();
-                    list.deleteAtPositon(pos);
-                    break;
-                case 7:
-                    list.viewList();
-                    break;
-                case 8:list.getListSize();break;
+                case 7:list.deleteFirst();break;
+                case 8:list.deleteLast();break;
                 case 9:
-                    System.out.println("thanks for using my program @bosscoder");
-                    System.exit(0);
+                    System.out.println("enter the position");
+                    pos = sc.nextInt();
+                    list.deleteAtPosition(pos);
+                    break;
+                case 10: list.reverse();break;
                 default:
-                    System.out.println("Invalid choice");
+                    System.out.println("invallid choice");
             }
         }
     }
 }
-
 
 class LinkedList{
-    private int size;
-    private Node start;
+        private Node start;
+        private int size;
 
-    public LinkedList(){
-        size = 0;
-        start = null;
-    }
-
-    public void insertAtFirst(int val){
-        Node n;
-        n = new Node();
-        n.setData(val);
-        n.setNext(start);
-        start = n;
-        size++;
-    }
-
-    public void insertAtLast(int val){
-        Node n,t;
-        n = new Node();
-        n.setData(val);
-        t = start;
-        if (t == null){
-            start = n;
-        }
-        else {
-            while (t.getNext() != null){
-                t = t.getNext();
-            }
-            t.setNext(n);
-        }
-        size++;
-    }
-
-    public void insertAtPosition(int val, int pos){
-        if (pos == 1){
-            insertAtFirst(val);
-        }else if (pos == size+1){
-            insertAtLast(val);
-        }else if (pos > 1 && pos <=size){
-            Node t,n;
-            n = new Node(val, null);
-            t = start;
-            for (int i = 1; i < pos-1; i++){
-                t = t.getNext();
-            }
-            n.setNext(t.getNext());
-            t.setNext(n);
-            size++;
-        }else{
-            System.out.println("Insertion not possible at position" + pos);
-        }
-    }
-
-    public void deleteFirst(){
-        if (start == null){
-            System.out.println("list is already empty");
-        }else {
-            start = start.getNext();
-            size--;
-        }
-    }
-
-    public void deleteLast(){
-        if (start == null){
-            System.out.println("list is already empty");
-        }else if (size == 1){
+        public LinkedList(){
             start = null;
-            size--;
-        }else{
+            size = 0;
+        }
+
+        public void insertAtStart(int val){
+            Node n;
+            n = new Node();
+            n.setData(val);
+            if (start == null){
+                start = n;
+                size++;
+            }else{
+                n.setNext(start);
+                start = n;
+                size++;
+            }
+        }
+
+        public void insertAtEnd(int val){
+            Node n,t;
+            n = new Node();
+            n.setData(val);
+            if (start ==  null){
+                start = n;
+                size++;
+            }else{
+                t= start;
+                for (int i = 1; i<size; i++){
+                    t = t.getNext();
+                }
+                t.setNext(n);
+                n.setNext(null);
+                size++;
+            }
+        }
+
+        public void insertAtPosition(int val, int pos){
+            if (pos < 0 || pos > size + 1){
+                System.out.println("invalid position");
+            }else if(start == null){
+                System.out.println("list is empty");
+            }
+            else if (pos == 1){
+                insertAtStart(val);
+            }else if (pos == size + 1){
+                    insertAtEnd(val);
+            }else{
+                Node n , t;
+                n = new Node(val, null);
+                n.setData(val);
+                n.setNext(null);
+                t = start;
+                for (int i = 1; i<pos-1; i++){
+                    t = t.getNext();
+                }
+                n.setNext(t.getNext());
+                t.setNext(n);
+                size++;
+            }
+        }
+
+        public void deleteFirst(){
+            if (start == null) {
+                System.out.println("list is already empty");
+            }else if(size == 1){
+                    start = null;
+            }else{
+                start = start.getNext();
+                size--;
+            }
+        }
+
+        public void deleteLast(){
+            if (start == null){
+                System.out.println("list is already empty");
+            }else if(size == 1){
+                start = null;
+            }else{
+                Node t;
+                t = start;
+                for (int i = 1; i<size-1; i++){
+                    t = t.getNext();
+                }
+                t.setNext(null);
+                size--;
+            }
+        }
+
+        public void deleteAtPosition(int pos){
+            if (pos<0 || pos>size){
+                System.out.println("invalid position");
+            }else if (pos == 1){
+                deleteFirst();
+            }else if (pos == size){
+                deleteLast();
+            }else{
+                Node t,t1;
+                t = start;
+                for (int i = 1; i<pos-1; i++){
+                    t = t.getNext();
+                }
+                t1 = t.getNext();
+                t.setNext(t1.getNext());
+                size--;
+            }
+        }
+
+        public void reverse(){
+            Node current = start;
+            Node previous = null;
+            Node n = null;
+            Node t;
+
+            if (start == null){
+                System.out.println("empty list cant't be reversed");
+            }
+            else{
+                while(current != null){
+                    n = current.getNext();
+                    current.setNext(previous);
+                    previous = current;
+                    current = n;
+                }
+
+                t = previous;
+
+                while (t != null){
+                    System.out.print(t.getData() + " ");
+                    t = t.getNext();
+                }
+                System.out.println();
+            }
+
+        }
+
+        public int getListSize(){
+            System.out.println("list size is : " + size);
+            return size;
+        }
+
+        public void view(){
             Node t;
             t = start;
-            for (int i = 1; i<size-1;i++){
-                t = t.getNext();
+            if (start == null){
+                System.out.println("no data in the list");
+            }else{
+                while (t != null){
+                    System.out.print(t.getData() + " ");
+                    t = t.getNext();
+                }
+                System.out.println();
             }
-            t.setNext(null);
-            size--;
         }
-    }
-
-    public void deleteAtPositon(int pos){
-        if (start == null){
-            System.out.println("List is already empty");
-        }else if (pos<1 || pos>size){
-            System.out.println("Invalid position");
-        }else if(pos == 1){
-            deleteFirst();
-        }else if(pos == size){
-            deleteLast();
-        }else{
-            Node t ,t1;
-            t = start;
-            for (int i = 1; i<pos-1; i++){
-                t = t.getNext();
-            }
-            t1 = t.getNext();
-            t.setNext(t1.getNext());
-            size--;
-        }
-    }
-
-    public boolean isEmpty(){
-        if (start == null){
-            return true;
-        }else{
-            return false;
-        }
-    }
-
-    public int getListSize(){
-        System.out.println("list size is : " + size);
-        return size;
-    }
-
-    public void viewList(){
-        Node t;
-        if (isEmpty()){
-            System.out.println("no data in the list");
-        }else{
-            t = start;
-            for (int i = 1; i<=size; i++){
-                System.out.print(" " + t.getData());
-                t = t.getNext();
-            }
-            System.out.println();
-        }
-    }
-
 
 }
 
-
-// node class
 class Node{
     private int data;
     private Node next;
@@ -207,21 +226,24 @@ class Node{
         data = 0;
         next = null;
     }
-    public Node(int d, Node n){
-        data = d;
+
+    public Node(int val, Node n){
+        data = val;
         next = n;
     }
 
-    public void setData(int d){
-        data = d;
-    }
-    public void setNext(Node n){
-        next = n;
+    public void setData(int val){
+        data = val;
     }
 
     public int getData(){
         return data;
     }
+
+    public void setNext(Node n){
+        next = n;
+    }
+
     public Node getNext(){
         return next;
     }

e5137f83b82a24dbf77cea988b9b0b349e84ecdc
skitwebdev <webdevelopmentskit@gmail.com>
2020-08-30 14:25:31 +0530

    length method added

--- src/com/bosscoder/SinglyLinkedList.java
+++ src/com/bosscoder/SinglyLinkedList.java
@@ -23,6 +23,19 @@ public class SinglyLinkedList {
         System.out.print("null");
     }
 
+    public int length() {
+        if (head == null) {
+            return 0;
+        }
+        int count = 0;
+        ListNode current = head;
+        while (current != null) {
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
+
     public static void main(String[] args) {
         SinglyLinkedList sll = new SinglyLinkedList();
         sll.head = new ListNode(10);
@@ -37,6 +50,8 @@ public class SinglyLinkedList {
         third.next = fourth;
 
         sll.display();
+        System.out.println();
+        System.out.println(sll.length());
     }
 
 }
